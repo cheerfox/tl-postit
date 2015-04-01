94bfb508cf835275b21992109ecff281db81f150
cheerfox <cheerfox@users.noreply.github.com>
2015-04-01 15:41:55 +0800

    Set the M-M asscoiation between Category and Post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331090811) do
+ActiveRecord::Schema.define(version: 20150331143516) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20150331090811) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "post_id"
+  end
+
+  create_table "post_categories", force: true do |t|
+    t.string   "post_id"
+    t.string   "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "posts", force: true do |t|
