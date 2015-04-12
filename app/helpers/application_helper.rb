094669b28c3d4cdccf62d4c24541e51ab81cc89b
cheerfox <cheerfox@users.noreply.github.com>
2015-04-12 19:54:58 +0800

    add display datetime helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def fix_url(str)
     str.start_with?("http://") ? str : "http://#{str}"
   end
+
+  def display_datetime(datetime)
+    datetime.strftime("%d/%m/%Y %H:%M %P")
+  end
 end
